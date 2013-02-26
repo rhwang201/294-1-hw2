926f6295df702955f2646040f4936b7a6370225e
Richard Hwang <richard.hwang201@gmail.com>
2013-02-25 16:49:34 -0800

    Adding some framework

--- hw2.scala
+++ hw2.scala
@@ -18,10 +18,24 @@ import BIDMat.Plotting._
 
 object RegressionModel {
 
-  val r_path = "/Users/richard/classes/294-1/hw2/data/tokenized.mat"
-  val d_path = "/Users/Davidius/294-1-hw2/data/tokenized.mat"
+  val data_file = "/Users/rhwang/classes/294-1/hw2/data/tokenized.mat"
+  //val data_file = "/Users/Davidius/294-1-hw2/data/tokenized.mat"
 
-  val data_file = "/Users/Davidius/294-1-hw2/data/tokenized.mat"
+  val processed_x_path = "/Users/richard/classes/294-1/hw2/data/processed.mat"
+
+  /** Processes the provided tokenized mat file into X and saves it. */
+  def process() = {
+    val tokens:IMat = load(data_file, "tokens")
+    val smap:CSMat=load(data_file, "smap")
+    val scnt:IMat=load(data_file, "scnt")
+  }
+
+  /** Calculates the L2 gradient at beta, where
+    *   L_2(beta) = E[-2y^T X + 2X^T X beta]
+    */
+  def l2_gradient(beta: BIDMat.SMat):Double = {
+    return 0
+  }
 
   /** Performs stochastic gradient descent (SGD) to minimize the L_2 loss
     * function, returning the vector beta of parameters. */
@@ -29,25 +43,35 @@ object RegressionModel {
     // Preprocess matrices
 
     // Perform SGD
-    sgd_tolerance = 0.0001
-    while (True) {
+    val sgd_tolerance = 0.0001
+    var beta = 0
+
+    while (true) {
       println("Not implemented")
       // Update
+      //beta = beta + gamma * l2_gradient(beta)
     }
   }
 
-  /** Returns the vector of predictions for input X, an nxd matrix. */
-  def predict() = {
+  /** Returns the vector of predictions for input X, an nxd matrix,
+    *   y_hat = beta_hat * X_hat */
+  def predict(beta: BIDMat.SMat, X: BIDMat.SMat) = {
+  }
+
+  /** Performs k-fold cross validation, computing AUC and 1% lift scores. */
+  def cross_validate(k: Int):Double = {
+    for (i <- 1 to k) {
+      // Split
+      // Train
+      // Test
+    }
+    // Compute measures
+    return 0
   }
 
   def main(args: Array[String]) = {
-    // Create model in blocks
-    //val tokens:IMat = load(data_file, "tokens")
-    //val smap:CSMat=load(data_file, "smap")
-
-    // TODO Can't load files
-    val scnt:CSMat=load(data_file, "scnt")
-    println("success")
+    process()
+    cross_validate(10)
   }
 
 }
